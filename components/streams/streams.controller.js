cc8ce0c9a9c45e0d1f0ffe1852739afa3fbc6dc4
mkze <mkzenicholson@gmail.com>
2015-11-28 04:09:06 -0500

    remove old non-service api calls
    previous commented out api calls removed

--- components/streams/streams.controller.js
+++ components/streams/streams.controller.js
@@ -51,42 +51,6 @@ StreamsController.prototype.watch = function (index) {
     }, function () {
         alert("Error retrieving stream access token");
     });
-
-    //$http.get("https://api.twitch.tv/api/channels/" + stream.channel.name + "/access_token").then(function (response) {
-
-    //    var token = response.data;
-
-
-    //    $http.get("http://usher.twitch.tv/api/channel/hls/" + stream.channel.name + ".m3u8?allow_source=true&allow_audio_only=true&type=any&private_code=null&player=twitchweb" + "&token=" + token.token + "&sig=" + token.sig + "&p=0420420").then(function (response) {
-    //        console.log(response);
-
-    //        var m3u_data = response.data;
-    //        var m3u_a = m3u_data.split("\n");
-    //        console.log(m3u_a);
-
-    //        m3u_a.forEach(function (line) {
-    //            if (line.indexOf("://") != -1 && line.indexOf("chunked") != -1) {
-    //                var url = line;
-    //                console.log(url);
-
-    //                var wcjs = require("wcjs-player");
-    //                var player = new wcjs("#player").addPlayer({ autoplay: true });
-    //                player.addPlaylist(url);
-    //                console.log(player);
-
-    //                document.getElementById("view").style.display = "none";
-
-    //            }
-    //        });
-
-    //    }, function () {
-    //        alert("error usher");
-    //    });
-
-
-    //}, function () {
-    //    alert("error token");
-    //});
 }
 
 
