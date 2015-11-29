f82bd9f8eb99bbdb7cee7332fe69bcffc7081fe0
mkze <mkzenicholson@gmail.com>
2015-11-28 20:24:34 -0500

    remove wcjs-player for wcjs-renderer
    - use wcjs-renderer module instead of wcjs-player
    - add window resize event handler for canvas
    - player and canvas markup & css modifications

--- js/player.service.js
+++ js/player.service.js
@@ -2,11 +2,48 @@
 "use strict";
 
 function PlayerService() {
+
+    var wcjs = require("wcjs-renderer");
+    var container = document.getElementById("player");
+    var canvasParent = document.getElementById("center");
+    var canvas = document.getElementById("canvas");
+
     return {
         createPlayer: function() {
-            var wcjs = require("wcjs-player");
-            var player = new wcjs("#player").addPlayer({ autoplay: true });
-            global.player = player;
+
+            if (!global.player) {
+                var player = wcjs.init(canvas);
+
+                // window resized event handler
+                window.onresize = function () {
+
+                    var destAspect = container.clientWidth / container.clientHeight;
+                    var sourceAspect = canvas.width / canvas.height;
+
+                    if (destAspect > sourceAspect) {
+                        canvasParent.style.height = "100%";
+                        canvasParent.style.width = (((container.clientHeight * sourceAspect) / container.clientWidth) * 100) + "%";
+                    } else {
+                        canvasParent.style.height = (((container.clientWidth / sourceAspect) / container.clientHeight) * 100) + "%";
+                        canvasParent.style.width = "100%";
+                    }
+
+                };
+
+                // create a mutation observer
+                var observer = new MutationObserver(function (mutations) {
+                    mutations.forEach(function (mutation) {
+                        window.onresize();
+                    });
+                });
+
+                // observe the canvas for attribute changes (width, height)
+                observer.observe(canvas, { attributes: true });
+
+                // set the created player as a property in the global object
+                global.player = player;
+            }
+
         },
         play: function (url) {
 
@@ -14,14 +51,19 @@ function PlayerService() {
             var video = document.getElementById("player");
             var view = document.querySelector("[ng-viewport]");
 
-            if (player.playing())
-                player.clearPlaylist();
+            //stop any existing playback
+            player.playlist.clear();
+            player.stop();
 
-            player.addPlaylist(url);
-            player.play();
+            //play url
+            player.play(url);
 
+            //toggle viewport/video
             view.hidden = true;
             video.hidden = false;
+            
+            //call resize event handler
+            window.onresize();
         }
     }
 }
